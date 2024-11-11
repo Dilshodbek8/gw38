81bf5eef5bb101b0656bb6a422960a07a181e923
Dilshodbek8 <20020905xd@gmail.com>
2024-11-11 17:28:45 +0500

    card qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ export default function App() {
   return (
     <>
       <h1>navbar</h1>
+      <h1>card1</h1>
+      <h1>card1</h1>
+      <h1>card1</h1>
+      <h1>card1</h1>
+      <h1>card1</h1>
+      <h1>card1</h1>
       <h1>Lorem ipsum dolor sit.</h1>
       <div className="bg-pink-400 p-10">
         <h1>footer</h1>
